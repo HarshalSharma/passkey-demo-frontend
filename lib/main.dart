78f508e6a098e23a056b668eb55bfaa4ffd69771
harshalsharma <er.harshalsharma@gmail.com>
2024-04-26 14:57:45 +0530

    added conclusion

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,9 @@ class _RootWidgetState extends State<RootWidget> {
           appBar: AppBar(
             title: Text(
               "Seamless Authentication Suite Demo",
-              style: screenSize.width > 500 ? AppConstants.textTheme.titleLarge : AppConstants.textTheme.titleSmall,
+              style: screenSize.width > 500
+                  ? AppConstants.textTheme.titleLarge
+                  : AppConstants.textTheme.titleSmall,
             ),
             leading: const Icon(Icons.vpn_key),
             backgroundColor: Colors.white,
@@ -217,9 +219,125 @@ class HomePage extends StatelessWidget {
                     "Step 2 - From authentication with known username, or \n"
                     "Step 6 - From authentication at preferred location after configuration."),
             MainDemo(
-              remoteServices: remoteServices,
-              passkeyService: passkeyService,
-              locationService: locationService
+                remoteServices: remoteServices,
+                passkeyService: passkeyService,
+                locationService: locationService),
+            const SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "CONCLUSION:",
+                style: AppConstants.textTheme.titleLarge,
+              ),
+            ),
+            const NumberedStep(
+                title:
+                    "It is demonstrated that the need to securely identify users to give access to their data can be easily full-filled with passkeys, without communicating with any third party services. "
+                    "\nThe User's of the websites/app also need not remember any new password to access their accounts and data securely on apps/websites that uses passkeys."
+                    "\n\nWe experimented to improve upon the current frictions of using passkeys, one of which is that users need to provide their username or email, Or the system has to use stored information from cookies to identify who is trying to login."),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text.rich(TextSpan(children: [
+                TextSpan(
+                  text: "What was Made: ",
+                  style: AppConstants.textTheme.bodyMedium!
+                      .copyWith(fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text:
+                      "We created an open-source library and a deployable Docker container. Any new website or app can use these tools to let users sign in with passkeys easily.",
+                  style: AppConstants.textTheme.bodyMedium,
+                ),
+              ])),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text.rich(TextSpan(children: [
+                TextSpan(
+                  text: "What is Learned: ",
+                  style: AppConstants.textTheme.bodyMedium!
+                      .copyWith(fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text:
+                      "We found out that it's possible to identify and let users sign in without needing usernames or cookies, just by knowing their current location. But this method has limitations, it may need other supporting factors and needs study.",
+                  style: AppConstants.textTheme.bodyMedium,
+                ),
+              ])),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text.rich(TextSpan(children: [
+                TextSpan(
+                  text: "Good Things About Passkey Authentication: ",
+                  style: AppConstants.textTheme.bodyMedium!
+                      .copyWith(fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text:
+                      "Users can create and access their accounts data on any website/apps without needing to remember any passwords or giving many details.",
+                  style: AppConstants.textTheme.bodyMedium,
+                ),
+              ])),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Pros and Cons of Using Home Location:",
+                style: AppConstants.textTheme.bodyLarge,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Good Points:",
+                style: AppConstants.textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.bold),
+              ),
+            ),
+            const NumberedStep(
+              number: "1",
+              title:
+                  "Users can easily sign in from home without needing usernames or passwords.",
+            ),
+            const NumberedStep(
+              number: "2",
+              title:
+                  "Very useful for elderly who don't remember any of their account names or passwords.",
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Not-So-Good Points:",
+                style: AppConstants.textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.bold),
+              ),
+            ),
+            const NumberedStep(
+              number: "1",
+              title:
+                  "You can only use this feature at home, not when you're away.",
+            ),
+            const NumberedStep(
+              number: "2",
+              title:
+                  "It might be slower in busy places with lots of people. example a tall flat buildings where many people have the same location.",
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Other Ideas:",
+                style: AppConstants.textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.bold),
+              ),
+            ),
+            const NumberedStep(
+                title:
+                    "Instead of using location, we could try something called device fingerprinting to solve the problems we found. But that comes with its own privacy and debate-able issues."),
+            const SizedBox(
+              height: 50,
             ),
             const Divider(
               height: 10,
@@ -229,16 +347,23 @@ class HomePage extends StatelessWidget {
               endIndent: 20,
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(16.0),
               child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "2024 \n Developed by Harshal Sharma \n (er.harshalsharma@gmail.com) \n"
-                    "Masters in Software Engineering \n BIRLA INSTITUTE OF TECHNOLOGY & SCIENCE, PILANI",
-                    textAlign: TextAlign.center,
-                    style: AppConstants.textTheme.bodySmall,
-                  ),
+                child: Text(
+                  "Thank you",
+                  textAlign: TextAlign.center,
+                  style: AppConstants.textTheme.titleLarge,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: Text(
+                  "2024 \n Developed by Harshal Sharma \n (er.harshalsharma@gmail.com) \n"
+                  "Masters in Software Engineering \n BIRLA INSTITUTE OF TECHNOLOGY & SCIENCE, PILANI",
+                  style: AppConstants.textTheme.bodySmall,
+                  textAlign: TextAlign.center,
                 ),
               ),
             )
